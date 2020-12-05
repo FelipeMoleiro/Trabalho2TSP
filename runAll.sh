80c53c3079e8e0713c3cdfcba5049aa011e3aa01
FelipeMoleiro <felipemoleiro@hotmail.com>
2020-12-05 20:28:41 -0300

    mais exemplos rodados

--- runAll.sh
+++ runAll.sh
@@ -42,17 +42,9 @@ python3 trab2CBC.py -t 1800 -hr 0 -se 2 -of output/uy/2.png < testCases/uy734.ts
 
 : 'Inicio dos testes com heuristica'
 
-
-python3 trab2CBC.py -t 1800 -hr 1 -se 0 -of output/wi/4.png < testCases/wi29.tsp > output/wi/4.txt &
-python3 trab2CBC.py -t 1800 -hr 1 -se 1 -of output/wi/5.png < testCases/wi29.tsp > output/wi/5.txt &
-python3 trab2CBC.py -t 1800 -hr 1 -se 2 -of output/wi/6.png < testCases/wi29.tsp > output/wi/6.txt &
-
-for job in `jobs -p`
-do
-echo $job
-    wait $job || let "FAIL+=1"
-done
-
+python3 trab2CBC.py -t 1800 -hr 1 -se 0 -of output/uy/4.png < testCases/uy734.tsp > output/uy/4.txt 
+python3 trab2CBC.py -t 1800 -hr 1 -se 1 -of output/uy/5.png < testCases/uy734.tsp > output/uy/5.txt 
+python3 trab2CBC.py -t 1800 -hr 1 -se 2 -of output/uy/6.png < testCases/uy734.tsp > output/uy/6.txt 
 
 : '
 python3 trab2CBC.py -t 1800 -hr 1 -se 0 -of output/wi/4.png < testCases/wi29.tsp > output/wi/4.txt &
@@ -65,7 +57,9 @@ echo $job
     wait $job || let "FAIL+=1"
 done
 '
-: '
+
+
+: ' FALTA RODAR
 python3 trab2CBC.py -t 1800 -hr 1 -se 0 -of output/dj/4.png < testCases/dj38.tsp > output/dj/4.txt &
 python3 trab2CBC.py -t 1800 -hr 1 -se 1 -of output/dj/5.png < testCases/dj38.tsp > output/dj/5.txt &
 python3 trab2CBC.py -t 1800 -hr 1 -se 2 -of output/dj/6.png < testCases/dj38.tsp > output/dj/6.txt &
