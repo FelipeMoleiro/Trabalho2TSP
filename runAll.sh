5ec9949514a7d1c8f0fe2b7ab6a0e8af48ab9fe6
FelipeMoleiro <felipemoleiro@hotmail.com>
2020-12-05 21:57:51 -0300

    mais umas rodads

--- runAll.sh
+++ runAll.sh
@@ -42,9 +42,15 @@ python3 trab2CBC.py -t 1800 -hr 0 -se 2 -of output/uy/2.png < testCases/uy734.ts
 
 : 'Inicio dos testes com heuristica'
 
-python3 trab2CBC.py -t 1800 -hr 1 -se 0 -of output/uy/4.png < testCases/uy734.tsp > output/uy/4.txt 
-python3 trab2CBC.py -t 1800 -hr 1 -se 1 -of output/uy/5.png < testCases/uy734.tsp > output/uy/5.txt 
-python3 trab2CBC.py -t 1800 -hr 1 -se 2 -of output/uy/6.png < testCases/uy734.tsp > output/uy/6.txt 
+python3 trab2CBC.py -t 1800 -hr 1 -se 0 -of output/qa/7.png < testCases/qa194.tsp > output/qa/7.txt &
+python3 trab2CBC.py -t 1800 -hr 1 -se 1 -of output/qa/8.png < testCases/qa194.tsp > output/qa/8.txt &
+python3 trab2CBC.py -t 1800 -hr 1 -se 2 -of output/qa/9.png < testCases/qa194.tsp > output/qa/9.txt &
+
+for job in `jobs -p`
+do
+echo $job
+    wait $job || let "FAIL+=1"
+done
 
 : '
 python3 trab2CBC.py -t 1800 -hr 1 -se 0 -of output/wi/4.png < testCases/wi29.tsp > output/wi/4.txt &
@@ -59,7 +65,7 @@ done
 '
 
 
-: ' FALTA RODAR
+: ' 
 python3 trab2CBC.py -t 1800 -hr 1 -se 0 -of output/dj/4.png < testCases/dj38.tsp > output/dj/4.txt &
 python3 trab2CBC.py -t 1800 -hr 1 -se 1 -of output/dj/5.png < testCases/dj38.tsp > output/dj/5.txt &
 python3 trab2CBC.py -t 1800 -hr 1 -se 2 -of output/dj/6.png < testCases/dj38.tsp > output/dj/6.txt &
